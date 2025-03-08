4ba70a82464010b7f2b2f02e331206ab1c3ebacc
Chase-520 <chace070528@gmail.com>
2025-03-07 21:45:36 -0500

    use the competiton control loop

--- cvdetection.py
+++ cvdetection.py
@@ -220,9 +220,9 @@ if __name__ == "__main__":
     import threading
     import time
     cam = cvCore()
-    cam_thread = threading.Thread(target=cam.control_loop_test,daemon=True)
+    cam_thread = threading.Thread(target=cam.control_loopt,daemon=True)
     cam_thread.start()
 
-    for i in range(100):
+    for i in range(120):
         print(f"time: {i}")
         time.sleep(1)
